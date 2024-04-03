1b90e7c5eb442ca7d8c97e74b4652f9c5cc1247e
python533 <143389231+python533@users.noreply.github.com>
2024-04-03 10:01:24 +0300

    CDM
    Cgi,os,cgitb

---
+++ cdm.py
@@ -0,0 +1,13 @@
+import os
+import cgi
+import cgitb
+
+form=''
+cgitb.enable()
+value=form.getlist()
+
+if not os.path:
+    f=open(dosya,rb)
+else:
+    <html>Dosya İndirme İşlemi <head> <meta charset="utf-8"></head><body> Aradığınız Dosya Yok</body></html>
+
